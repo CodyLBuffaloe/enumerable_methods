31e83306f0f9940ad4cf58878a37d177bf931caa
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-05-02 18:06:52 -0400

    Completed my_each and My_each_with_index

---
+++ enumerable_methods.rb
@@ -0,0 +1,39 @@
+module Enumerable
+
+    def my_each(&block)
+
+        i = 0
+
+        while(i < self.size )
+
+            yield(self[i])
+
+            i += 1
+
+        end
+
+        self
+
+    end
+
+    def my_each_with_index(&block)
+
+        index = 0
+
+        while(index < self.size)
+
+            yield(self[index], index)
+
+            index += 1
+
+        end
+
+        self
+
+    end
+
+    def my_select(&block)
+    end
+
+
+end
