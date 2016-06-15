8d4d23b47733791c6b451e16eb378ca1ed145375
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-06-15 11:38:48 -0700

    Finished #my_all method, although it doesn't return nil. Not sure under what circumstances #all? returns nil, either.

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -3,7 +3,7 @@ module Enumerable
     def my_each(&block)
 
         i = 0
-
+# use a for loop when incrementing a subscript
         while(i < self.size )
 
             yield(self[i])
@@ -42,6 +42,30 @@ module Enumerable
 
         self
 
+    end
+
+    def my_all?(&block)
+
+        are_all_true = false
+
+            self.my_each do |element|
+
+                if block_given?
+
+                    are_all_true = yield(element)
+
+                    if (are_all_true == false)
+                        return are_all_true
+                        break
+                    end
+                else
+                    self.my_each do |element| element
+                        are_all_true = true
+                    end
+                end
+
+            end
+        return are_all_true
     end
 
 
