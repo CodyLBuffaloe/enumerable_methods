ff80406240cac4e757710faef43847e5e9c49909
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-06-20 16:22:24 -0700

    Almost finished #my_any? method. Cleaned up code smells in #my_all?

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -32,13 +32,13 @@ module Enumerable
 
     end
 
-    def my_select(&block)
+    def my_select(&block) #returns all true els from a set
         all_true_els = []
        self.my_each do |element|
             return enum_for(:my_select) unless block_given?
             this_el = yield(element)
             if (this_el == true)
-                all_true_els << element 
+                all_true_els << element
 
             end
 
@@ -61,14 +61,29 @@ module Enumerable
                         break
                     end
                 else
-                    self.my_each do |element| element
-                        are_all_true = true
-                    end
+                    are_all_true = true
                 end
 
             end
         return are_all_true
     end
 
+    def my_any?(&block) # As long as not all els are false, will return true
+        are_any_true = false
+
+        self.my_each do |element|
+
+            if(block_given? || element == true)
+                are_any_true = yield(element)
+                if(are_any_true == true)
+                    return are_any_true = true
+                    break
+                end
+            end
+
+        end
+
+        return are_any_true
+    end
 
 end
