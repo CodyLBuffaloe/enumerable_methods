41032ee599a5b34888d936e34c21674076ddffdc
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-06-22 12:43:25 -0700

    Finished adding #my_any? functionality.

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,59 +1,47 @@
 module Enumerable
 
     def my_each(&block)
-
         i = 0
-# use a for loop when incrementing a subscript
-        while(i < self.size )
 
+        while(i < self.size )# use a for loop when incrementing a subscript
             yield(self[i])
-
             i += 1
-
         end
 
         self
-
     end
 
     def my_each_with_index(&block)
-
         index = 0
 
         while(index < self.size)
-
             yield(self[index], index)
-
             index += 1
-
         end
 
         self
-
     end
 
     def my_select(&block) #returns all true els from a set
         all_true_els = []
+
        self.my_each do |element|
             return enum_for(:my_select) unless block_given?
             this_el = yield(element)
             if (this_el == true)
                 all_true_els << element
-
             end
-
        end
+
         return all_true_els
     end
 
     def my_all?(&block)
-
         are_all_true = false
         if block_given?
             self.my_each do |element|
 
                     are_all_true = yield(element)
-
                     if (are_all_true == false)
                         return are_all_true
                         break
@@ -74,21 +62,23 @@ module Enumerable
 
     def my_any?(&block) # As long as not all els are false, will return true
         are_any_true = false
+        if block_given?
+            self.my_each do |element|
 
-        self.my_each do |element|
-
-            if block_given?
-                are_any_true = yield(element)
-                if(are_any_true == true)
+                 are_any_true = yield(element)
+                 if(are_any_true == true)
+                        are_any_true = true
+                        break
+                 end
+            end
+        else
+            self.my_each do |element|
+                if(element == true)
                     are_any_true = true
                     break
                 end
-            else
-
             end
-
         end
-
         return are_any_true
     end
 
